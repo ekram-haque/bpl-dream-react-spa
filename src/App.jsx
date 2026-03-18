a38eecae4efa5dd30f298d70f8c93cbd3e8ae915
ekram-haque <dev.ekramul@gmail.com>
2026-03-18 08:29:06 +0600

    create basic stureture of project and install tailwindCss and daisyUi

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+<div className="bg-white text-black min-h-screen">
+<h1 className="text-center  ">bpl dream project is cooking......</h1>
+
+</div>
+  )
+
+  
+}
+
+export default App;
